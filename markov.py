e43c9c3c5a5122aa70b5742d67d17b59e422f894
Xander <xandermay1@gmail.com>
2019-10-12 08:32:42 -0400

    Part 3 slide 42

--- markov.py
+++ markov.py
@@ -21,6 +21,7 @@ def generate_bot_answer_with_text_model(text_model):
     
     word_list = user_question.split(" ")
     random_word = random.choice(word_list)
+    bot_answer = text_model.make_sentence_with_start()
 
 
       
