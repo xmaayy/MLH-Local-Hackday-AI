65e7910fef668add9d0a69dd4ad085dfa4b2833f
Xander <xandermay1@gmail.com>
2019-10-12 08:28:41 -0400

    Part 3 slide 36

--- markov.py
+++ markov.py
@@ -4,6 +4,7 @@ from mlh_twitter_api import get_user_tweets as fetch
 from twitter_scraper_fetcher import *
 import re
 
+
 # remove emoji, punctuation, urls from tweets
 def create_string(tweets):
     text = ""
@@ -14,10 +15,12 @@ def create_string(tweets):
         )  # Make sure each tweet is handled properly by markovify
     return text
 
+
 def generate_bot_answer_with_text_model(text_model):
     pass
     ## write code here
-        
+
+      
 # build the markov chain based on the text we read
 # we use the markovify library to do this step
 def generate_bot_answer(twitter_handle, user_question):
